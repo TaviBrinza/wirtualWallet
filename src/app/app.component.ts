140283078b97224ccf1b756fe67fcbfbf83b9ebd
Octavian Brinza <octavian.brinza@yahoo.com>
2021-12-14 15:58:57 +0200

    new file:   .browserslistrc 	new file:   .editorconfig 	new file:   .gitignore 	new file:   README.md 	new file:   angular.json 	new file:   karma.conf.js 	new file:   package-lock.json 	new file:   package.json 	new file:   src/app/app.component.css 	new file:   src/app/app.component.html 	new file:   src/app/app.component.spec.ts 	new file:   src/app/app.component.ts 	new file:   src/app/app.module.ts 	new file:   src/app/components/coin/coin.component.css 	new file:   src/app/components/coin/coin.component.html 	new file:   src/app/components/coin/coin.component.spec.ts 	new file:   src/app/components/coin/coin.component.ts 	new file:   src/app/components/dashboard/dashboard.component.css 	new file:   src/app/components/dashboard/dashboard.component.html 	new file:   src/app/components/dashboard/dashboard.component.spec.ts 	new file:   src/app/components/dashboard/dashboard.component.ts 	new file:   src/app/components/footer/footer.component.css 	new file:   src/app/components/footer/footer.component.html 	new file:   src/app/components/footer/footer.component.spec.ts 	new file:   src/app/components/footer/footer.component.ts 	new file:   src/app/components/header/header.component.css 	new file:   src/app/components/header/header.component.html 	new file:   src/app/components/header/header.component.spec.ts 	new file:   src/app/components/header/header.component.ts 	new file:   src/app/services/coins.service.spec.ts 	new file:   src/app/services/coins.service.ts 	new file:   src/assets/.gitkeep 	new file:   src/assets/images/logo.png 	new file:   src/environments/environment.prod.ts 	new file:   src/environments/environment.ts 	new file:   src/favicon.ico 	new file:   src/index.html 	new file:   src/main.ts 	new file:   src/polyfills.ts 	new file:   src/styles.css 	new file:   tsconfig.app.json 	new file:   tsconfig.json 	new file:   tsconfig.spec.json

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { CoinsService } from './services/coins.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Virtual-Wallet';
+  coins : any =[];
+  constructor(private coinsService:CoinsService){
+
+  }
+  ngOnInit():void {
+    this.getCoinsData();
+
+  }
+
+  getCoinsData(){
+     this.coinsService.getCoins().subscribe((response) => {
+       this.coins = response;
+     })
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { CoinComponent } from './components/coin/coin.component';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    DashboardComponent,
+    CoinComponent,
+
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
